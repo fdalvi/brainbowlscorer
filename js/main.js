665fb935539a9a65a69a3787c24822ee85b39dec
Fahim Imaduddin Dalvi <fid@qatar.cmu.edu>
2014-01-18 20:40:44 +0300

    Basic webpage design complete. Reveal modal integrated.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+	// Determine the current Semester and Year
+	var currentDate = new Date();
+
+	if(currentDate.getMonth() < 6)
+		$("h2").html("Spring " + currentDate.getFullYear());
+	else
+		$("h2").html("Fall " + currentDate.getFullYear());
+});
+
+function revealAbout()
+{
+	$('#about').reveal({
+     animation: 'fadeAndPop',                   //fade, fadeAndPop, none
+     animationspeed: 300,                       //how fast animtions are
+     closeonbackgroundclick: true,              //if you click background will modal close?
+     dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
+ });
+}
