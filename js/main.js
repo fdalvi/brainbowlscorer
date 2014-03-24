c1e55cb3c1cc1092ee0ff10b884daf332fc03ae5
Fahim Dalvi <fahim@dyn-172-20-69-186.qatar.cmu.local>
2014-03-25 01:01:32 +0300

    Basic functionality is now complete.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,53 @@ $( document ).ready(function() {
 		$("h2").html("Spring " + currentDate.getFullYear());
 	else
 		$("h2").html("Fall " + currentDate.getFullYear());
+
+	$(".increaseScore").click(function() {
+		var currentScore = parseInt($(this).parent().children(".score")[0].innerHTML);
+		$(this).parent().children(".score")[0].innerHTML = (currentScore+1) +  '' ;
+	});
+
+	$(".decreaseScore").click(function() {
+		var currentScore = parseInt($(this).parent().children(".score")[0].innerHTML);
+		$(this).parent().children(".score")[0].innerHTML = (currentScore-1) +  '' ;
+	});
 });
 
 function revealAbout()
 {
 	$('#about').reveal({
-     animation: 'fadeAndPop',                   //fade, fadeAndPop, none
-     animationspeed: 300,                       //how fast animtions are
-     closeonbackgroundclick: true,              //if you click background will modal close?
-     dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
- });
+		animation: 'fadeAndPop',                   //fade, fadeAndPop, none
+		animationspeed: 300,                       //how fast animtions are
+		closeonbackgroundclick: true,              //if you click background will modal close?
+		dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
+	});
+}
+
+function toggleEditMode()
+{
+	console.log($(".teamname").attr('contentEditable') );
+	if($(".teamname").attr('contentEditable') == undefined || $(".teamname").attr('contentEditable') == 'false')
+	{
+		$(".teamname").attr('contentEditable','true');
+		$(".teamname").addClass('shake');
+		//$(".teamname").addClass('bordered');
+	}
+	else
+	{
+		$(".teamname").attr('contentEditable','false');
+		$(".teamname").removeClass('shake');
+		//$(".teamname").removeClass('bordered');
+	}
+}
+
+function revealSettings()
+{
+	$('#settings-dialog').reveal({
+		animation: 'fadeAndPop',                   //fade, fadeAndPop, none
+		animationspeed: 300,                       //how fast animtions are
+		closeonbackgroundclick: true,              //if you click background will modal close?
+		dismissmodalclass: 'close-reveal-modal'    //the class of a button or element that will close an open modal
+	});
 }
 
 function showResources() {
